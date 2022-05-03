dd9a6abf96c1fb5da3ec1492f70892fa690e45f0
Dylan Beaudette <debeaudette@ucdavis.edu>
2022-05-03 15:51:02 -0700

    exploring the use of shannon entropy to describe conceptual precision

---
+++ SSA-complexity/MU-entropy.R
@@ -0,0 +1,158 @@
+library(aqp)
+library(latticeExtra)
+library(viridis)
+library(tactile)
+library(hexbin)
+
+
+equal.prob.H <- function(n, b = 2) {
+  p <- rep(1, times = n) / n
+  shannonEntropy(p, b = b)
+}
+
+epH <- Vectorize(equal.prob.H)
+
+
+## double-check: YES
+## H_2(p) / H_2_max(n) == H_n(p)
+
+# p <- c(0.2, 0.2, 0.6)
+# shannonEntropy(p, b = 2) / equal.prob.H(n = 3, b = 2)
+# shannonEntropy(p, b = 3)
+
+## question: does the distribution of H/H_max == distribution of quantile(H/H_max) ?
+
+## how to incorporate area? H/area, by delineation?
+
+
+x <- read.csv('entropy-by-mukey.csv.gz')
+y <- read.csv('entropy-by-mukey-statsgo.csv.gz')
+
+
+x$mukind <- sprintf("SSURGO\n%s", x$mukind)
+y$mukind <- sprintf("STATSGO\n%s", y$mukind)
+
+# stack
+vars <- c('areasymbol', 'projectscale', 'mukind', 'mukey', 'entropy', 'n')
+g <- make.groups(SSURGO = x[, vars], STATSGO = y[, vars])
+
+row.names(g) <- NULL
+
+head(g)
+
+table(g$which, g$mukind, useNA = 'always')
+
+summary(g$entropy)
+
+
+# investigate these
+# ~ 22% 0-entropy
+# some related to dropped misc. area components
+prop.table(table(g$entropy == 0))
+
+# single component MU
+# misc. areas
+# https://casoilresource.lawr.ucdavis.edu/soil_web/list_components.php?mukey=1425970
+z <- g[g$entropy == 0, ]
+head(z)
+
+
+# remove 0-entropy for now
+g <- subset(g, subset = entropy > 0)
+
+(scale.tab <- sort(round(prop.table(table(g$projectscale)), 3)))
+
+
+# equal-prob H
+g$H.max <- epH(g$n)
+
+# check: negative values suggests H > H_max
+idx <- which(sign(g$H.max - g$entropy) == -1)
+length(idx)
+
+# these are all tiny numbers
+g[idx, ]$entropy - g[idx, ]$H.max
+
+
+
+
+tps <- tactile.theme(plot.symbol = list(cex = 0.5))
+
+
+histogram( ~ entropy | which, data = g, par.settings = tps, breaks = 100, scales = list(alternating = 1, x = list(tick.number = 10), y = list(alternating = 3)), xlab = 'Shannon Entropy (base 2)')
+
+histogram( ~ entropy / H.max | which, data = g, par.settings = tps, breaks = 100, scales = list(alternating = 1, x = list(tick.number = 10), y = list(alternating = 3)), xlab = 'Shannon Entropy / Equal-Probability Entropy')
+
+bwplot(which ~ entropy / H.max, data = g, par.settings = tps, xlab = 'Shannon Entropy / Equal-Probability Entropy', main = 'All Map Unit Kinds', scales = list(x = list(tick.number = 10)), xlim = c(-0.1, 1.1))
+
+bwplot(which ~ entropy, data = g, par.settings = tps, xlab = 'Shannon Entropy (base 2))', main = 'All Map Unit Kinds', scales = list(x = list(tick.number = 10)))
+
+
+
+p1 <- bwplot(mukind ~ entropy, 
+             data = g, 
+             par.settings = tps, 
+             xlab = 'Shannon Entropy (base 2)', 
+             main = '', 
+             scales = list(x = list(tick.number = 10)), 
+             varwidth = FALSE, 
+             panel = function(...) {
+               panel.grid(h = 0, v = -1)
+               panel.bwplot(...)
+             }
+)
+
+
+p2 <- bwplot(mukind ~ entropy / H.max, 
+             data = g, 
+             par.settings = tps, 
+             xlab = 'Shannon Entropy / Equal-Probability Entropy', 
+             main = '', 
+             scales = list(x = list(tick.number = 10)), 
+             varwidth = FALSE, 
+             panel = function(...) {
+               panel.grid(h = 0, v = -1)
+               panel.bwplot(...)
+             }
+)
+
+
+ragg::agg_png(filename = 'mukind-entropy-example.png', width = 1000, height = 600, scaling = 1.5)
+
+print(p1, split = c(1, 1, 1, 2), more = TRUE)
+print(p2, split = c(1, 2, 1, 2), more = FALSE)
+
+dev.off()
+
+
+g.sub <- subset(g, subset = areasymbol %in% c('tx027', 'ca630', 'ca113', 'ca792', 'US'))
+g.sub$areasymbol <- factor(g.sub$areasymbol, levels = c('tx027', 'ca630', 'ca113', 'ca792', 'US'))
+
+bwplot(areasymbol ~ entropy / H.max, data = g.sub, par.settings = tps, xlab = 'Shannon Entropy / Equal-Probability Entropy', main = '',  scales = list(x = list(tick.number = 10)), varwidth = FALSE, xlim = c(-0.1, 1.1))
+
+bwplot(areasymbol ~ entropy, data = g.sub, par.settings = tps, xlab = 'Shannon Entropy (base 2)', main = '', scales = list(x = list(tick.number = 10)), varwidth = FALSE)
+
+
+
+
+
+bwplot(mukind ~ entropy, data = g, subset = projectscale %in% c(12000, 24000), par.settings = tps, xlab = 'Shannon Entropy (base 2)', main = '1:12,000 and 1:24,000 Surveys', scales = list(x = list(tick.number = 10)))
+
+bwplot(mukind ~ entropy / H.max, data = g, subset = projectscale %in% c(12000, 24000), par.settings = tps, xlab = 'Shannon Entropy / Equal-Probability Entropy', main = '1:12,000 and 1:24,000 Surveys', scales = list(x = list(tick.number = 10)), xlim = c(-0.1, 1.1))
+
+
+
+# hard to interpret because not all scales are well-represented
+bwplot(factor(projectscale) ~ entropy, data = g, subset = projectscale %in% c(12000, 24000, 250000), par.settings = tps, xlab = 'Shannon Entropy (base 2)', scales = list(x = list(tick.number = 10)))
+
+
+
+
+# # blah
+# hexbinplot(entropy ~ projectscale, data = x, subset = projectscale < 100000, trans = log, inv = exp, colramp = viridis, colorkey = FALSE, asp = 1, scales = list(x = list(log = 10)), xscale.components = xscale.components.log10.3)
+# 
+# 
+# ## nothing here
+# hexbinplot(entropy ~ coryear, data = x, trans = log, inv = exp, colramp = viridis, colorkey = FALSE)
+# 
+
---
+++ SSA-complexity/entropy-by-mukey.sql
@@ -0,0 +1,143 @@
+-- Compute Shanon Entropy 
+
+
+--
+-- Of course we don't need to use area... because we are talking about proportions
+--
+ 
+-- Notes:
+-- * excluding all misc. areas --> misc. area map units with a single component have very low entropy... not a useful indicator
+-- * 
+ 
+ 
+ 
+\timing
+
+-- EXPLAIN
+
+
+-- ~ 3 minutes
+
+DROP TABLE IF EXISTS h;
+CREATE TEMP TABLE h AS 
+
+-- total component pct
+WITH component_p AS (
+-- areasymbol is not citext in most ssurgo.* tables, just lower-case
+SELECT mapunit.areasymbol::citext AS areasymbol, 
+mukind, mukey, comppct_r / 100.0 AS comppct
+FROM ssurgo.mapunit 
+JOIN ssurgo.component USING (mukey)
+WHERE comppct_r IS NOT NULL
+AND mukind != ''
+AND compkind != 'Miscellaneous area'
+AND compkind != ''
+),
+-- total by mukey
+total_p AS (
+SELECT mukey, SUM(comppct) AS sum_comppct
+FROM component_p
+GROUP BY mukey
+),
+-- proportions
+proportions AS (
+SELECT 
+component_p.areasymbol, component_p.mukind, component_p.mukey, comppct / sum_comppct AS p
+FROM component_p JOIN total_p USING (mukey)
+)
+-- entropy
+-- also
+SELECT
+areasymbol, coryear, projectscale, mukind, mukey, 
+-- note: log(0) --> error
+-- either add fuzz or remove
+ROUND(- SUM(p * LOG(2.0, p::numeric)), 6) AS entropy,
+-- number of components
+COUNT(p) AS n
+FROM proportions
+JOIN soilweb.ssurgo_status USING (areasymbol)
+-- test this: retain only real proportions
+WHERE p > 0
+GROUP BY areasymbol, coryear, projectscale, mukind, mukey ;
+
+
+
+--
+-- statsgo
+--
+DROP TABLE IF EXISTS hs;
+CREATE TEMP TABLE hs AS 
+
+
+-- total component pct
+WITH component_p AS (
+SELECT mukind, mukey, comppct_r / 100.0 AS comppct
+FROM statsgo.mapunit 
+JOIN statsgo.component USING (mukey)
+WHERE comppct_r IS NOT NULL
+AND mukind != ''
+AND compkind != 'Miscellaneous area'
+AND compkind != ''
+),
+-- total by mukey
+total_p AS (
+SELECT mukey, SUM(comppct) AS sum_comppct
+FROM component_p
+GROUP BY mukey
+),
+-- proportions
+proportions AS (
+SELECT 
+component_p.mukind, component_p.mukey, comppct / sum_comppct AS p
+FROM component_p JOIN total_p USING (mukey)
+)
+-- entropy
+-- also
+SELECT
+-- double-check / think about scale
+'US' AS areasymbol, 250000::integer AS projectscale, mukind, mukey, 
+-- note: log(0) --> error
+-- either add fuzz or remove
+ROUND(- SUM(p * LOG(2.0, p::numeric)), 6) AS entropy,
+-- number of components
+COUNT(p) AS n
+FROM proportions
+-- test this: retain only real proportions
+WHERE p > 0
+GROUP BY areasymbol, projectscale, mukind, mukey ;
+
+
+\copy h TO 'entropy-by-mukey.csv' CSV HEADER
+\copy hs TO 'entropy-by-mukey-statsgo.csv' CSV HEADER
+
+
+
+--
+-- checking
+--
+
+
+-- aqp::shannonEntropy(base = 2) --> 2.788091
+-- computed above: ----------------> 2.788091
+--
+-- select mukey, cokey, ROUND(comppct_r / 100.0, 3) FROM ssurgo.component where mukey = '2766854';
+
+-- s <- c(0.35, 0.1, 0.03, 0.07, 0.15, 0.15, 0.04, 0.05, 0.05, 0.01)
+-- sum(s)
+-- shannonEntropy(s)
+
+--
+-- entropy of 0:
+--
+
+-- single component, 85%
+-- SELECT mukey, cokey, ROUND(comppct_r / 100.0, 3) FROM ssurgo.component where mukey = '1425970';
+
+-- 100% misc. area
+-- SELECT mukey, compkind, cokey, ROUND(comppct_r / 100.0, 3) FROM ssurgo.component where mukey = '618801';
+
+-- single component, 100%
+-- SELECT mukey, compkind, cokey, ROUND(comppct_r / 100.0, 3) FROM ssurgo.component where mukey = '54021';
+
+
+
