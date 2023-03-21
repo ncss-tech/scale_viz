cb0b535a7a00537381e6523b207fcd0a45d2e520
Stephen Roecker <stephen.roecker@gmail.com>
2023-03-21 09:37:56 -0500

    Update scale_calculations.R

--- scale_calculations.R
+++ scale_calculations.R
@@ -9,6 +9,13 @@ mlra$m2     <- st_area(mlra)
 statsgo$m2  <- st_area(statsgo)
 statsgo$cm2 <- units::set_units(statsgo$m2, cm^2)
 
+
+# minimum legible delineation
+mld_cm2 <- units::set_units(c(0.01, 0.10, 0.25, 0.40, 0.60), cm^2)
+mld_m2  <- units::set_units(mld_cm2, m^2)
+mld_ha  <- units::set_units(mld_cm2, ha)
+mld_ac  <- units::set_units(mld_cm2, acre)
+
 # definitions
 ## map scale
 ms <- 1 / 24000
@@ -20,8 +27,8 @@ ms <- 1 / 24000
 rf <- 1 / 24000
 
 # scale nummber
-sn <- 24000
-
+SN <- 24000
+SN2 <- c(`SSURGO 1st` = 12000, `SSURGO 2nd` = 24000, STATSGO = 250000, LRU = 1000000, MLRA = 3500000, LRR = 7500000)
 
 sn <- function(MLA, MLD) {
 
@@ -34,16 +41,19 @@ sn <- function(MLA, MLD) {
   # }
 }
 
-p <- c(0, 0.05, 0.1)
-sn(quantile(statsgo$m2, p), mld_m2[3])
-sn(quantile(mlra$m2, p), mld_m2[3])
+p <- c(0, 0.05, 0.1, 0.5, 0.95, 1)
+sn(quantile(statsgo$m2, p), mld_m2[4])
+sn(quantile(mlra$m2, p), mld_m2[4])
+
+
+SN1 <- as.numeric(sn(MLA = c(10, 30, 100)^2, mld_m2[4]))
+names(SN1) <- c("10-meter", "30-meter", "100-meter")
+SN2 <- c(SN1, SN2)
+SN2
 
 
 # function of delineation size
 
-# minimum legible delination
-mld_cm2 <- units::set_units(c(0.10, 0.25, 0.40), cm^2)
-mld_m2  <- units::set_units(mld_cm2, m^2)
 
 
 # minimum legible area 
@@ -51,11 +61,13 @@ mla <- function(SN, MLD) {
   SN^2 * MLD
 }
 
+formatC(mla(SN2, mld_ac[4]), format)
 mla(20000, mld_m2)
+mla(24000, mld_m2)
+mla(24000, mld_ac)
 
 
-
-## average size of delination
+## average size of delineation
 asd <- function(area, n) {
   sum(area) /  n
 }
@@ -80,4 +92,15 @@ sci <- function(perimeter, area) {
 
 
 asd(statsgo$cm2 * 10000, nrow(statsgo))
+
+
+# table for printing
+
+ac <- format(signif(as.numeric(mla(SN2, mld_ac[4])), 2), scientific = FALSE, big.interval = ",")
+ha <- format(signif(as.numeric(mla(SN2, mld_ha[4])), 2), scientific = FALSE, big.interval = ",")
+m2 <- format(signif(as.numeric(mla(SN2, mld_m2[4])), 2), scientific = FALSE, big.interval = ",")
+m  <- signif(sqrt(as.numeric(m2)), 2)
+df <- data.frame(SN = signif(as.numeric(SN2), 2), `MLD ac` = ac, `MLD ha` = ha, `MLD m2` = m2, `MLD m` =  m)
+df <- cbind(`Soil Map` = names(SN2), df)
+df
 
