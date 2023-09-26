c002a6054a93b07562d47384505f943ceb321554
Beaudette <dylan.beaudette@usda.gov>
2023-09-26 11:07:47 -0700

    exclude STATSGO

--- invesintens-SSURGO.R
+++ invesintens-SSURGO.R
@@ -4,7 +4,7 @@ library(soilDB)
 # total number of map units
 SDA_query("SELECT COUNT(mukey) as n FROM mapunit")
 
-
+# exclude STATSGO
 .sql <- "
 SELECT CASE WHEN invesintens IS NULL THEN 'missing' ELSE invesintens END, 
 CAST(
@@ -14,6 +14,8 @@ CAST(
   AS numeric
 ) / (SELECT COUNT(mukey) FROM mapunit) AS prop
 FROM mapunit
+INNER JOIN legend ON mapunit.lkey = legend.lkey
+WHERE areasymbol != 'US'
 GROUP BY invesintens 
 ORDER BY prop DESC ;"
 
