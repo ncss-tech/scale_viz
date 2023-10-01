b06aa489358af358a9e8b9f955d96250d45925b5
Beaudette <dylan.beaudette@usda.gov>
2023-09-30 21:48:19 -0700

    Update invesintens-SSURGO.R

--- invesintens-SSURGO.R
+++ invesintens-SSURGO.R
@@ -1,5 +1,5 @@
 library(soilDB)
-
+library(terra)
 
 # total number of map units
 SDA_query("SELECT COUNT(mukey) as n FROM mapunit")
@@ -23,3 +23,33 @@ z <- SDA_query(.sql)
 
 
 knitr::kable(z, digits = 3)
+
+###
+
+# make a bounding box and assign a CRS (4326: GCS, WGS84)
+a <- vect('POLYGON((-118.7220 36.4500,-118.7220 36.5746,-118.4678 36.5746,-118.4678 36.4500,-118.7220 36.4500))', crs = 'epsg:4326')
+
+# fetch gSSURGO map unit keys at native resolution (30m)
+mu <- mukey.wcs(aoi = a, db = 'gssurgo')
+
+# extract RAT for thematic mapping
+rat <- cats(mu)[[1]]
+
+.sql <- sprintf(
+  "SELECT mukey, invesintens FROM mapunit WHERE mukey IN %s ;", 
+  format_SQL_in_statement(as.integer(rat$mukey))
+)
+
+tab <- SDA_query(.sql)
+
+rat <- merge(rat, tab, by = 'mukey', all.x = TRUE, sort = FALSE)
+
+table(rat$invesintens)
+
+# re-pack rat
+levels(mu) <- rat
+
+activeCat(mu) <- 'invesintens'
+plot(mu, axes = FALSE, col = hcl.colors(n = 4, palette = 'spectral'))
+
+
